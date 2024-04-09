20d4683765414b74857f576e0d51894e063be726
Sergey <127251969+Sergey2007-png@users.noreply.github.com>
2024-04-09 09:13:06 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,76 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+ 
+
+  });
+
+
+  var swiper = new Swiper(".mySwipers", {
+    slidesPerView: 7,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+   
+  });
+
+  var swiper = new Swiper(".mySwiperr", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  $(function(){
+    $('.menu-burger_log').on('click', function(){
+    $('.header_phone').slideToggle();
+    });
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".mySwiperr", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
